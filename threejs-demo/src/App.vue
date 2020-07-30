6f786668e2bfedbaea2c1a2d9a4f1f1359a99fe3
liuyaxiong <yaxiong.liu@aqara.com>
2020-07-30 17:47:35 +0800

    <div>feat:ops2</div>

--- threejs-demo/src/App.vue
+++ threejs-demo/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div>feat:ops</div>
     <div>feat:ops1</div>
+    <div>feat:ops2</div>
     <router-link :to="{ name: 'Page1' }"> page1 </router-link>
     <router-view> </router-view>
   </div>
